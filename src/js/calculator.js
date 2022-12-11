9c1be9b2eda2f95f2c276b6d8003bf1027693c62
mwojcickaa <martynawojcickaaa@gmail.com>
2022-12-11 19:40:14 +0100

    Fix the code

--- src/js/calculator.js
+++ src/js/calculator.js
@@ -26,7 +26,6 @@ let result;
 const valueOperatorArray = [];
 
 function myValueAndOperator(value) {
-    // valueOperatorArray.push(value)
     if (typeof value === "string") {
         if (typeof valueOperatorArray[valueOperatorArray.length - 1] === "string") {
             valueOperatorArray.splice(valueOperatorArray.length - 1, 1, value)
@@ -42,67 +41,69 @@ function myValueAndOperator(value) {
             valueOperatorArray.push(value)
         }
     }
+}
 
-    let currentNumber = valueOperatorArray.join('')
-    document.getElementById('currentNumber').innerHTML = currentNumber;
+let currentNumber = valueOperatorArray.join('')
+document.getElementById('currentNumber').innerHTML = currentNumber;
 
-    buttonNumberOne.addEventListener("click", function () { myValueAndOperator(1) })
+buttonNumberOne.addEventListener("click", function () { myValueAndOperator(1) })
 
-    buttonNumberTwo.addEventListener("click", function () { myValueAndOperator(2) })
+buttonNumberTwo.addEventListener("click", function () { myValueAndOperator(2) })
 
-    buttonNumberThree.addEventListener("click", function () { myValueAndOperator(3) })
+buttonNumberThree.addEventListener("click", function () { myValueAndOperator(3) })
 
-    buttonNumberFour.addEventListener("click", function () { myValueAndOperator(4) })
+buttonNumberFour.addEventListener("click", function () { myValueAndOperator(4) })
 
-    buttonNumberFive.addEventListener("click", function () { myValueAndOperator(5) })
+buttonNumberFive.addEventListener("click", function () { myValueAndOperator(5) })
 
-    buttonNumberSix.addEventListener("click", function () { myValueAndOperator(6) })
+buttonNumberSix.addEventListener("click", function () { myValueAndOperator(6) })
 
-    buttonNumberSeven.addEventListener("click", function () { myValueAndOperator(7) })
+buttonNumberSeven.addEventListener("click", function () { myValueAndOperator(7) })
 
-    buttonNumberEight.addEventListener("click", function () { myValueAndOperator(8) })
+buttonNumberEight.addEventListener("click", function () { myValueAndOperator(8) })
 
-    buttonNumberNine.addEventListener("click", function () { myValueAndOperator(9) })
+buttonNumberNine.addEventListener("click", function () { myValueAndOperator(9) })
 
-    buttonNumberZero.addEventListener("click", function () { myValueAndOperator(0) },)
+buttonNumberZero.addEventListener("click", function () { myValueAndOperator(0) },)
 
-    buttonDot.addEventListener("click", function () { myValueAndOperator(".") })
+buttonDot.addEventListener("click", function () { myValueAndOperator(".") })
 
-    additionSign.addEventListener("click", function () { myValueAndOperator("+") })
-    subtractionSign.addEventListener("click", function () { myValueAndOperator("-") })
-    multiplicationSign.addEventListener("click", function () { myValueAndOperator("*") })
-    divisionSign.addEventListener("click", function () { myValueAndOperator("/") })
+additionSign.addEventListener("click", function () { myValueAndOperator("+") })
+subtractionSign.addEventListener("click", function () { myValueAndOperator("-") })
+multiplicationSign.addEventListener("click", function () { myValueAndOperator("*") })
+divisionSign.addEventListener("click", function () { myValueAndOperator("/") })
 
-    equalsSign.addEventListener("click", function () {
-        if (firstValue != undefined, secondValue != undefined) {
-            if (operator === "+") {
-                result = Number(firstValue) + Number(secondValue)
-                document.getElementById('currentNumber').innerHTML = "";
-                document.getElementById('currentNumber').innerHTML = result;
-            }
-            else if (operator === "-") {
-                result = Number(firstValue) - Number(secondValue)
-                document.getElementById('currentNumber').innerHTML = "";
-                document.getElementById('currentNumber').innerHTML = result;
-            }
-            else if (operator === "*") {
-                result = Number(firstValue) * Number(secondValue)
-                document.getElementById('currentNumber').innerHTML = "";
-                document.getElementById('currentNumber').innerHTML = result;
-            }
-            else if (operator === "/") {
-                result = Number(firstValue) / Number(secondValue)
-                document.getElementById('currentNumber').innerHTML = "";
-                document.getElementById('currentNumber').innerHTML = result;
-            }
+equalsSign.addEventListener("click", function () {
+    if (firstValue != undefined, secondValue != undefined) {
+        if (operator === "+") {
+            result = Number(firstValue) + Number(secondValue)
+            document.getElementById('currentNumber').innerHTML = "";
+            document.getElementById('currentNumber').innerHTML = result;
         }
-    })
+        else if (operator === "-") {
+            result = Number(firstValue) - Number(secondValue)
+            document.getElementById('currentNumber').innerHTML = "";
+            document.getElementById('currentNumber').innerHTML = result;
+        }
+        else if (operator === "*") {
+            result = Number(firstValue) * Number(secondValue)
+            document.getElementById('currentNumber').innerHTML = "";
+            document.getElementById('currentNumber').innerHTML = result;
+        }
+        else if (operator === "/") {
+            result = Number(firstValue) / Number(secondValue)
+            document.getElementById('currentNumber').innerHTML = "";
+            document.getElementById('currentNumber').innerHTML = result;
+        }
+    }
+})
 
-    clearSign.addEventListener("click", function () {
-        document.getElementById('currentNumber').innerHTML = "";
-        firstValue = undefined
-        secondValue = undefined
-        operator = undefined
-    })
+clearSign.addEventListener("click", function () {
+    document.getElementById('currentNumber').innerHTML = "";
+    firstValue = undefined
+    secondValue = undefined
+    operator = undefined
+})
+
 
 
